b9de7746f2f8277b251996cbe55a3725be42a5f9
hadaryatzkan-hub <hadaryatzkan@gmail.com>
2026-05-31 12:22:14 +0300

    auto: save changes

--- src/lib/emailConfig.ts
+++ src/lib/emailConfig.ts
@@ -1,4 +1,4 @@
 export const EMAILJS_PUBLIC_KEY = "uu7Ebb1xoRVC4rajz";
 export const EMAILJS_SERVICE_ID = "service_gjyfw7a";
-export const EMAILJS_CONTACT_TEMPLATE = "template_84p0myq";
-export const EMAILJS_FEE_TEMPLATE = "template_89ub4fn";
+export const EMAILJS_CONTACT_TEMPLATE = "template_89ub4fn";
+export const EMAILJS_FEE_TEMPLATE = "template_84p0myq";
